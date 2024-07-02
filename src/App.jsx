5fc316fe1590f578eb72d7b87f1d2ae6cf721465
ganesh-gandla <ganeshgandla19@gmail.com>
2024-07-02 13:36:13 +0530

    provided local storage to save the input data

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,58 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from '../components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [form, setForm] = useState({ Site: "", UserName: "", Password: "" })
+  const [passwordArray, setPasswordArray] = useState([])
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if (passwords) {
+      setPasswordArray(JSON.parse(passwords))
+    }
+  }, [])
 
   const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const savePassword = () => {
+    setPasswordArray([...passwordArray, form])
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
+    { console.log([...passwordArray, form]) }
 
   }
 
   return (
     <>
-     <Navbar/>
-     <div className="password-generator">
-      <input type="text" placeholder='Enter the site' onChange={e => handleChange(e)}/>
-      <input type="text" placeholder='Enter the User Name' onChange={e => handleChange(e)}/>
-      <input type="text" placeholder='Enter the Password' onChange={e => handleChange(e)}/>
-      <button>Save Password</button>
-     </div>
+      <Navbar />
+      <div className="password-generator">
+        <input type="text" placeholder='Enter the site' name="Site" onChange={handleChange} />
+        <input type="text" placeholder='Enter the User Name' name="UserName" onChange={handleChange} />
+        <input type="text" placeholder='Enter the Password' name="Password" onChange={handleChange} />
+        <button onClick={savePassword}>Save Password</button>
+      </div>
+      <h2>Your Passwords</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Site</th>
+            <th>User Name</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {passwordArray.map((item) => {
+            return <tr>
+              <td>{item.Site}</td>
+              <td>{item.UserName}</td>
+              <td>{item.Password}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+
     </>
   )
 }
